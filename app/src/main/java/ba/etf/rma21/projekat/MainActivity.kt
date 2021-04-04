1b9d910c50af4ab46cddb830f24dd66874649b7e
Tarik Horozovic <thorozovic1@etf.unsa.ba>
2021-04-04 19:26:30 +0200

    create QuizAdapter and VM

--- app/src/main/java/ba/etf/rma21/projekat/MainActivity.kt
+++ app/src/main/java/ba/etf/rma21/projekat/MainActivity.kt
@@ -2,13 +2,22 @@ package ba.etf.rma21.projekat
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import ba.etf.rma21.projekat.view.QuizAdapter
+import ba.etf.rma21.projekat.viewmodel.QuizListViewModel
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var listaKvizova : RecyclerView
+    private lateinit var kvizAdapter : QuizAdapter
+    private var kvizViewModel = QuizListViewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        listaKvizova = findViewById(R.id.listaKvizova)
+        listaKvizova.adapter = kvizAdapter
+
     }
 }
 
---
+++ app/src/main/java/ba/etf/rma21/projekat/view/QuizAdapter.kt
@@ -0,0 +1,37 @@
+package ba.etf.rma21.projekat.view
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import ba.etf.rma21.projekat.R
+import ba.etf.rma21.projekat.data.models.Kviz
+
+class QuizAdapter (private var kvizovi : List<Kviz>) : RecyclerView.Adapter<QuizAdapter.QuizViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): QuizViewHolder {
+        val view = LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.quiz_item, parent, false)
+        return QuizViewHolder(view)
+    }
+
+
+    inner class QuizViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val nazivPredmeta : TextView = itemView.findViewById(R.id.nazivPredmeta)
+        val nazivKviza : TextView = itemView.findViewById(R.id.nazivPredmeta)
+        val datumKviza : TextView = itemView.findViewById(R.id.nazivPredmeta)
+        val trajanje : TextView = itemView.findViewById(R.id.nazivPredmeta)
+        val osvojeniBodovi : TextView = itemView.findViewById(R.id.nazivPredmeta)
+        val status : ImageView = itemView.findViewById(R.id.status)
+    }
+
+    override fun onBindViewHolder(holder: QuizViewHolder, position: Int) {
+//        holder.nazivKviza =
+    }
+
+    override fun getItemCount(): Int {
+        return kvizovi.size
+    }
+}
