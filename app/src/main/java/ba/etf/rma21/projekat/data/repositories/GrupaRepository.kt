b2d2321bf413d3314fabf7d50667d7cc805ff462
Tarik Horozovic <thorozovic1@etf.unsa.ba>
2021-04-13 00:02:36 +0200

    Implementirana metoda getGroupsByPredmet

--- app/src/main/java/ba/etf/rma21/projekat/data/repositories/GrupaRepository.kt
+++ app/src/main/java/ba/etf/rma21/projekat/data/repositories/GrupaRepository.kt
@@ -1,6 +1,7 @@
 package ba.etf.rma21.projekat.data.repositories
 
 import ba.etf.rma21.projekat.data.models.Grupa
+import ba.etf.rma21.projekat.data.models.Predmet
 import ba.etf.rma21.projekat.data.staticData.StaticGrupa
 
 class GrupaRepository {
@@ -8,8 +9,13 @@ class GrupaRepository {
         var sveGrupe : List<Grupa> = StaticGrupa.kreirajGrupe()
 
         fun getGroupsByPredmet(nazivPredmeta: String): List<Grupa> {
-            // TODO: Implementirati
-            return emptyList()
+            val grupePoPredmetu : MutableList<Grupa> = emptyList<Grupa>().toMutableList()
+            for (grupa in sveGrupe)
+            {
+                if (grupa.nazivPredmeta == nazivPredmeta)
+                    grupePoPredmetu.add(grupa)
+            }
+            return grupePoPredmetu.toList()
         }
     }
 }
